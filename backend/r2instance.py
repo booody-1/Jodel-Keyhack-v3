17bcb47ae3ce939fa7f964b85f20cd93ddf8ce5a
krokofant <krokofant@users.noreply.github.com>
2018-12-27 17:48:12 +0100

    Fix import

--- backend/r2instance.py
+++ backend/r2instance.py
@@ -2,12 +2,14 @@ from collections import OrderedDict
 
 import r2pipe
 import re
+from decrypt import CLIENT_SECRET_SIZE
 
 R2_LIST_FUNCTIONS = 'afl'
 R2_DISASSEMBLE_INSTRUCTIONS = 's {}; pi 25'
 
 REGEX_EXTRACT_BYTES = r'(?<=[^ ] )\d\w*'
 REGEX_FIND_FUNCTIONS = r'sym.\w+Hmac\w+init'
+
 
 def rev(a):
     new = ""
@@ -16,12 +18,12 @@ def rev(a):
 
     return new
 
+
 class R2Instance:
     def __init__(self, path):
         self.r2 = r2pipe.open(path)
         self.r2.cmd('aa')
         self.is_correct_binary = False
-
 
         method_name = self.get_method_name()
         if method_name is not None:
@@ -51,13 +53,15 @@ class R2Instance:
     def extract_bytes(self):
         instr = {}
         # https://memegenerator.net/img/instances/75909642/how-does-this-even-work.jpg
-        instructions = [d for d in self.r2.cmd(R2_DISASSEMBLE_INSTRUCTIONS.format(self.function_name)).split('\r') if 'mov' in d and 'eax' in d]
+        instructions = [d for d in self.r2.cmd(R2_DISASSEMBLE_INSTRUCTIONS.format(
+            self.function_name)).split('\r') if 'mov' in d and 'eax' in d]
         for i in instructions:
             matches = re.findall(REGEX_EXTRACT_BYTES, i)
-            value = matches[1].replace('0x','').strip()
-            if len(value) <= 1 or (8 > len(value) > 2): value = '0' + value
-            if len(value) > 8 and value.startswith('0'): value = value[1:]
+            value = matches[1].replace('0x', '').strip()
+            if len(value) <= 1 or (8 > len(value) > 2):
+                value = '0' + value
+            if len(value) > 8 and value.startswith('0'):
+                value = value[1:]
             instr[int(matches[0], 0)] = rev(value)
 
-        return [int(''.join(OrderedDict(sorted(instr.items())).values())[x:x + 2], 16) for x in range(0, decrypt.CLIENT_SECRET_SIZE*2, 2)]
-
+        return [int(''.join(OrderedDict(sorted(instr.items())).values())[x:x + 2], 16) for x in range(0, CLIENT_SECRET_SIZE*2, 2)]
